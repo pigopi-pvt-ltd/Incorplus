c53dc108237172affd4f0bc8c90697767298932e
milan-2025 <milandeepbhalla@outlook.com>
2026-01-24 19:56:45 +0530

    meta updated

--- src/Components/Layouts/RootLayout.jsx
+++ src/Components/Layouts/RootLayout.jsx
@@ -1,13 +1,30 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
-import { useScrollToTop } from "@/hooks/useScrollToTop";
-import WhatsAppChat from "../CommonComponents/WhatsAppChat";
-
+import { Outlet, useLocation } from "react-router-dom"
+import Navbar from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
+import { useScrollToTop } from "@/hooks/useScrollToTop"
+import WhatsAppChat from "../CommonComponents/WhatsAppChat"
+import ReactPixel from "react-facebook-pixel"
+import { useEffect } from "react"
 // const { default: Navbar } = require("../Navbar/Navbar");
 
 const RootLayout = ({}) => {
-  useScrollToTop();
+  const location = useLocation()
+
+  // Initialize Pixel only once when the app loads
+  useEffect(() => {
+    const options = {
+      autoConfig: true,
+      debug: false,
+    }
+    ReactPixel.init("827976683581212", options)
+  }, [])
+
+  // Fire a "PageView" event every time the location changes
+  useEffect(() => {
+    ReactPixel.pageView()
+    // console.log('Tracking page view:', location.pathname); // Uncomment to debug
+  }, [location])
+  useScrollToTop()
   return (
     <>
       <div className="bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-100 font-sans antialiased max-w-full overflow-x-clip selection:bg-primary-500 selection:text-white">
@@ -20,7 +37,7 @@ const RootLayout = ({}) => {
         <WhatsAppChat />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default RootLayout;
+export default RootLayout
